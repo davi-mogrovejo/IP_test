51da48fcdacf68aa63585ee7cef889bf883f88a5
davi-mogrovejo <124883755+davi-mogrovejo@users.noreply.github.com>
2023-09-07 14:22:13 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,43 @@
-// Function to fetch and display the user's IP address
-async function getIpAddress() {
-    try {
-        const response = await fetch('https://api.ipify.org?format=json');
-        const data = await response.json();
-        const ipAddress = data.ip;
+// Function to get the local IP address
+function getLocalIpAddress() {
+    return new Promise((resolve, reject) => {
+        const RTCPeerConnection = window.RTCPeerConnection || window.mozRTCPeerConnection || window.webkitRTCPeerConnection;
+        const peerConnection = new RTCPeerConnection({ iceServers: [] });
 
-        // Update the IP address on the webpage
-        document.getElementById('ip-address').textContent = ipAddress;
-    } catch (error) {
-        console.error('Error fetching IP address:', error);
-        document.getElementById('ip-address').textContent = 'Error';
-    }
+        peerConnection.createDataChannel('');
+
+        peerConnection.onicecandidate = (event) => {
+            if (!event.candidate) {
+                // All ICE candidates have been gathered
+                peerConnection.close();
+                resolve(null);
+            } else {
+                const localIpRegex = /\d+\.\d+\.\d+\.\d+/;
+                const match = localIpRegex.exec(event.candidate.candidate);
+                if (match) {
+                    const localIpAddress = match[0];
+                    peerConnection.close();
+                    resolve(localIpAddress);
+                }
+            }
+        };
+
+        // Create an offer and set the local description
+        peerConnection.createOffer()
+            .then((offer) => peerConnection.setLocalDescription(offer))
+            .catch(reject);
+    });
 }
 
-// Call the function when the page loads
-window.addEventListener('load', getIpAddress);
+// Call the function to get the local IP address
+getLocalIpAddress()
+    .then((localIp) => {
+        if (localIp) {
+            console.log('Local IP address:', localIp);
+        } else {
+            console.log('Local IP address not found.');
+        }
+    })
+    .catch((error) => {
+        console.error('Error getting local IP address:', error);
+    });
