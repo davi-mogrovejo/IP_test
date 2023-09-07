572cad7dc8f6c382a6cec9e10b28a83b67d91d3b
davi-mogrovejo <124883755+davi-mogrovejo@users.noreply.github.com>
2023-09-07 16:04:52 -0300

    Update sendip.php

--- sendip.php
+++ sendip.php
@@ -3,10 +3,15 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $ipv4 = $_POST['ipv4'];
     $ipv6 = $_POST['ipv6'];
 
-    $to = 'davi.mogrovejo@gmail.com';
+    $to = 'qwer_fadf@gmail.com';
     $subject = 'New IP Addresses';
     $message = "IPv4 Address: $ipv4\nIPv6 Address: $ipv6";
 
-    mail($to, $subject, $message);
+    if (mail($to, $subject, $message)) {
+        echo 'Email sent successfully';
+    } else {
+        echo 'Email delivery failed';
+        error_log(error_get_last()['message']); // Log the error message
+    }
 }
 ?>
