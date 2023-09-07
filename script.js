193057a1ef49d9737915e4af23ae8516e97d89f4
davi-mogrovejo <124883755+davi-mogrovejo@users.noreply.github.com>
2023-09-07 15:58:16 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,25 +1,52 @@
 // script.js
+function sendIPAddresses(ipv4, ipv6) {
+    const data = { ipv4, ipv6 };
+    
+    fetch('sendip.php', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(data),
+    })
+    .then(response => {
+        if (response.ok) {
+            console.log('IP addresses sent successfully');
+        } else {
+            console.error('Failed to send IP addresses');
+        }
+    })
+    .catch(err => {
+        console.error('Error sending IP addresses:', err);
+    });
+}
+
 function getIPAddresses() {
     // Fetch IPv6 address
     fetch('https://api64.ipify.org?format=json')
         .then(response => response.json())
         .then(data => {
-            document.getElementById('ipv6').textContent = data.ip;
+            const ipv6 = data.ip;
+            document.getElementById('ipv6').textContent = ipv6;
+            
+            // Fetch IPv4 address
+            fetch('https://api.ipify.org?format=json')
+                .then(response => response.json())
+                .then(data => {
+                    const ipv4 = data.ip;
+                    document.getElementById('ipv4').textContent = ipv4;
+                    
+                    // Send IP addresses to the server
+                    sendIPAddresses(ipv4, ipv6);
+                })
+                .catch(err => {
+                    console.error('Error fetching IPv4 address:', err);
+                    document.getElementById('ipv4').textContent = 'N/A';
+                });
         })
         .catch(err => {
             console.error('Error fetching IPv6 address:', err);
             document.getElementById('ipv6').textContent = 'N/A';
-        });
-
-    // Fetch IPv4 address
-    fetch('https://api.ipify.org?format=json')
-        .then(response => response.json())
-        .then(data => {
-            document.getElementById('ipv4').textContent = data.ip;
-        })
-        .catch(err => {
-            console.error('Error fetching IPv4 address:', err);
-            document.getElementById('ipv4').textContent = 'N/A';
         });
 }
 
