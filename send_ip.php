ba9996d5188653f99d1641c95135b050cdc460e0
davi-mogrovejo <124883755+davi-mogrovejo@users.noreply.github.com>
2023-09-07 14:38:10 -0300

    Update send_ip.php

--- send_ip.php
+++ send_ip.php
@@ -1,17 +1,1 @@
-<?php
-$ipAddress = $_SERVER['REMOTE_ADDR']; // Get the user's IP address
 
-// Your email configuration
-$to = 'davi.mogrovejo@gmail.com'; // Replace with your email address
-$subject = 'User IP Address';
-$message = 'The user\'s IP address is: ' . $ipAddress;
-$headers = 'From: davi.mogrovejo@gmail.com' . "\r\n" .
-   'Reply-To: your_email@example.com' . "\r\n" .
-   'X-Mailer: PHP/' . phpversion();
-
-// Send the email
-mail($to, $subject, $message, $headers);
-
-// Redirect the user back to the webpage
-header('https://davi-mogrovejo.github.io/IP_test/'); // Replace with the actual URL of your webpage
-?>
